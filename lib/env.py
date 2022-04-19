5885355dc7719e859aa14412acb7a3a88e6fc63f
IT-gMA <tiendatoan@gmail.com>
2022-04-19 21:24:24 +0800

    allowing for setting  within the code file instead of using the console

--- lib/env.py
+++ lib/env.py
@@ -1,10 +1,14 @@
 import os
 from pathlib import Path
+import unittest
+from mock import patch
 
-ABSOLUTE_PATH = r"C:\Users\tiend\PycharmProjects\tabular-dl"
-PROJECT_DIR = Path(os.environ[ABSOLUTE_PATH]).absolute().resolve()
-DATA_DIR = PROJECT_DIR / 'data'
-OUTPUT_DIR = PROJECT_DIR / 'output'
+ABSOLUTE_PATH = r"/Users/tiend/PycharmProjects/tabular-dl"
+# set the project directory within the code file instead of via the console
+with patch.dict('os.environ', {"PROJECT_DIR": ABSOLUTE_PATH}):
+    PROJECT_DIR = Path(os.environ['PROJECT_DIR']).absolute().resolve()
+    DATA_DIR = PROJECT_DIR / 'data'
+    OUTPUT_DIR = PROJECT_DIR / 'output'
 
 
 def get_path(relative_path: str) -> Path:
