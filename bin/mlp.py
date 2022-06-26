60adbafc54f442ce2960e6fd934cae8eee000d49
IT-gMA <tiendatoan@gmail.com>
2022-06-26 18:26:13 +0800

    change loss function to mish

--- bin/mlp.py
+++ bin/mlp.py
@@ -120,7 +120,7 @@ epoch_size = stats['epoch_size'] = math.ceil(train_size / batch_size)
 )'''
 #new loss function with mse or mish
 loss_fn = (
-    F.mse_loss
+    F.mish_loss
 )
 
 args['model'].setdefault('d_embedding', None)
